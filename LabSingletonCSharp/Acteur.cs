8c9ccba35d85582093011a30b2f94f53578613a2
Francis <super_f_qc@hotmail.com>
2015-10-02 18:02:19 -0400

    jh

---
+++ LabSingletonCSharp/Acteur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSingletonCSharp
+{
+    public class Acteur:IDisposable
+    {
+        private static int cptAct = 0;
+        public string nom { get; private set; }
+        public string prenom { get; private set; }
+
+        public Acteur(string nom)
+        {
+            this.nom = nom.Trim().Split(' ')[0];
+            this.prenom = nom.Trim().Split(' ')[1];
+            cptAct++;
+        }
+
+        public static int NbrActeur() 
+        {
+            return cptAct;
+        }
+
+        public Acteur(Acteur act)
+        {
+            cptAct++;
+        }
+
+        public void Dispose()
+        {
+            cptAct--;
+            //Dispose(true);
+        }
+       
+    }
+}
---
+++ LabSingletonCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LabSingletonCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fichier = File.OpenText("C:/Users/201250541/Documents/Client_Serveur/LaboSingletons/ListeActeur3.txt");
+            List<Acteur> Acteurs = new List<Acteur>(); 
+            while (fichier.Peek()>=0)
+	        {
+                Acteurs.Add(new Acteur(fichier.ReadLine()));
+	        }
+            Console.WriteLine(Acteur.NbrActeur());
+
+            asd();
+            Acteurs.Remove(Acteurs.First());
+
+            Console.WriteLine(Acteur.NbrActeur());
+        }
+
+        static private void asd()
+        {
+            using(Acteur asd = new Acteur("sedf sdf"))
+            {
+                Console.WriteLine(Acteur.NbrActeur());
+            }
+            
+        }
+    }
+}
